ae55bed5035422ca953848b0e7042354e897b69d
clcnsy <clcnsy@gmail.com>
2013-11-15 00:41:10 +0800

    Initial Commit

---
+++ NCTransformXmlBean/NCTransformXmlBean.h
@@ -0,0 +1,13 @@
+//
+//  NCTransformXmlBean.h
+//  NCTransformXmlBean
+//
+//  Created by clcnsy on 13-11-15.
+//  Copyright (c) 2013年 Nan.Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NCTransformXmlBean : NSObject
+
+@end
---
+++ NCTransformXmlBean/NCTransformXmlBean.m
@@ -0,0 +1,13 @@
+//
+//  NCTransformXmlBean.m
+//  NCTransformXmlBean
+//
+//  Created by clcnsy on 13-11-15.
+//  Copyright (c) 2013年 Nan.Chen. All rights reserved.
+//
+
+#import "NCTransformXmlBean.h"
+
+@implementation NCTransformXmlBean
+
+@end
